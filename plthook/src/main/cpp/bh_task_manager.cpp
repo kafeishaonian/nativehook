b89f38cea91ab57f9324969d486bdfae95cdb41a
kafeishaonian <648600445@qq.com>
2024-01-19 14:49:28 +0800

    task_manager

--- plthook/src/main/cpp/bh_task_manager.cpp
+++ plthook/src/main/cpp/bh_task_manager.cpp
@@ -54,10 +54,101 @@ void bh_task_manager_delete(bh_task_manager_t *self, bh_task_t *task) {
 }
 
 
-void bh_task_manager_hook(bh_task_manager_t *self, bh_task_t *task) {
+static void bh_task_manager_post_new_elf(bh_elf_t *elf, void *arg) {
+    BH_LOG_INFO("task manager: try hook in new ELF: %s", elf->pathname);
+    bh_task_manager_t *self = static_cast<bh_task_manager_t *>(arg);
 
+    pthread_rwlock_rdlock(&self->lock);
+    bh_task_t *task;
+    TAILQ_FOREACH(task, &self->tasks, link) {
+        bh_task_hook_elf(task, elf);
+    }
+    pthread_rwlock_unlock(&self->lock);
+}
+
+static void bh_task_manager_post_dlopen(void *arg) {
+    BH_LOG_INFO("task manager: post dlopen() OK");
+
+    bh_dl_monitor_dlclose_rdlock();
+    bh_elf_manager_refresh(bh_core_global()->elf_mgr, false, bh_task_manager_post_new_elf, arg);
+    bh_dl_monitor_dlclose_unlock();
+}
+
+static void bh_task_manager_post_dlclose(bool sync_refresh, void *arg) {
+    (void)arg;
+    BH_LOG_INFO("task manager: post dlclose() ok, sync_refresh: %d", sync_refresh);
+
+    if (sync_refresh) {
+        bh_elf_manager_refresh(bh_core_global()->elf_mgr, true, NULL, NULL);
+    } else {
+        bh_dl_monitor_dlclose_rdlock();
+        bh_elf_manager_refresh(bh_core_global()->elf_mgr, false, NULL, NULL);
+        bh_dl_monitor_dlclose_unlock();
+    }
+}
+
+static int bh_task_manager_init_dl_monitor(bh_task_manager_t *self) {
+    static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+    static bool inited = false;
+    static bool inited_ok = false;
+
+    if (inited) {
+        return inited_ok ? 0 : -1;
+    }
+
+    int r;
+    pthread_mutex_lock(&lock);
+    if (!inited) {
+        bh_dl_monitor_set_post_dlopen(bh_task_manager_post_dlopen, self);
+        bh_dl_monitor_set_post_dlclose(bh_task_manager_post_dlclose, NULL);
+        if (0 == (r = bh_dl_monitor_init())) {
+            inited_ok = true;
+        }
+        inited = true;
+    } else {
+        r = inited_ok ? 0 : -1;
+    }
+    pthread_mutex_unlock(&lock);
+    return r;
+}
+
+void bh_task_manager_hook(bh_task_manager_t *self, bh_task_t *task) {
+    if (bh_dl_monitor_is_initing()) {
+        static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+        static bool oneshot_refreshed = false;
+        if (!oneshot_refreshed) {
+            bool hooked = false;
+            pthread_mutex_lock(&lock);
+            if (!oneshot_refreshed) {
+                bh_dl_monitor_dlclose_rdlock();
+                bh_elf_manager_refresh(bh_core_global()->elf_mgr, false, NULL, NULL);
+                bh_task_hook(task);
+                bh_dl_monitor_dlclose_unlock();
+                oneshot_refreshed = true;
+                hooked = true;
+            }
+            pthread_mutex_unlock(&lock);
+            if (hooked) {
+                return;
+            }
+        }
+    } else {
+        if (0 != bh_task_manager_init_dl_monitor(self)) {
+            bh_task_hooked(task, PLT_HOOK_STATUS_CODE_INITERR_DLMTR, NULL, NULL);
+            return;
+        }
+    }
+
+    bh_dl_monitor_dlclose_rdlock();
+    bh_task_hook(task);
+    bh_dl_monitor_dlclose_unlock();
 }
 
 int bh_task_manager_unhook(bh_task_manager_t *self, bh_task_t *task) {
+    (void)self;
 
+    bh_dl_monitor_dlclose_rdlock();
+    int r = bh_task_unhook(task);
+    bh_dl_monitor_dlclose_unlock();
+    return r;
 }
