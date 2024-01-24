91d228e549b46bfabd85ee0e10ffddbc4da860d5
kafeishaonian <648600445@qq.com>
2024-01-24 16:17:29 +0800

    task

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -11,8 +11,40 @@ S
 #plthook/src/main/cpp/CMakeLists.txt,e/a/ea5a02ad0ef87684645efaddfad2dbac77b25582
 V
 &plthook/src/main/cpp/include/plthook.h,b/8/b82d8f73ad3e208d86cf956946ae83c0912f56bb
+\
+,plthook/src/main/cpp/third_party/bsd/queue.h,6/7/679f806c3d7fca4c3c581fc50e1a2f5fe8f9aeeb
+[
++plthook/src/main/cpp/third_party/bsd/tree.h,c/2/c2ad0a578c39b513ed33c759167ab3530a2ca500
+R
+"plthook/src/main/cpp/bh_trampo.cpp,3/8/38ef2af6dbe42624b424478264ccfbaa9d719932
+P
+ plthook/src/main/cpp/bh_trampo.h,5/a/5a2045d2f8461fd7536d4bd4a6c5d3982f857b07
+V
+&plthook/src/main/cpp/bh_trampo_arm.cpp,f/e/fe992a99254cb14d9973a77bfe268cd230686a7f
+X
+(plthook/src/main/cpp/bh_trampo_arm64.cpp,9/f/9fae64d2a89c5bb72f85857a53ad9fd663c9112b
+V
+&plthook/src/main/cpp/bh_trampo_x86.cpp,4/3/436a4967f961d15594c80cfd3d526f64c648aa8b
+Y
+)plthook/src/main/cpp/bh_trampo_x86_64.cpp,7/2/72cf99df954c2b144b9386d5de2833280c956ad4
+P
+ plthook/src/main/cpp/bh_util.cpp,5/1/51eda040c8fbe18f026feed9584d90dbe82cbd8c
+N
+plthook/src/main/cpp/bh_util.h,f/8/f88236864b9fa17e4fcd45bea58e13cec7f14f86
+P
+ plthook/src/main/cpp/bytesig.cpp,4/0/402a3874b9eacedd4424ea5b2317fa23a4455e89
+N
+plthook/src/main/cpp/bytesig.h,4/a/4aab1d00beae6879c58b7e828113d88cedca3d5d
+P
+ plthook/src/main/cpp/plthook.cpp,d/e/de4430fe0cd9f06596fec9dea443aac7c0d4b3ce
+T
+$plthook/src/main/cpp/plthook_jni.cpp,a/d/ad4e50a2e4b324100f1bcf413e5aa87c507f6d48
+O
+plthook/src/main/cpp/bh_cfi.cpp,9/d/9dc3769b9589f3da2ff36a04ef81a0f6392a216d
 M
 plthook/src/main/cpp/bh_cfi.h,9/e/9e608f007b6ad992fe64defdcefd5a67f4879002
+O
+plthook/src/main/cpp/bh_const.h,2/f/2fee577aab4f94f62bbb7f4b16ff79ef96af96e0
 P
  plthook/src/main/cpp/bh_core.cpp,9/4/944cf4885f2c3234c1cd9549ca404b6aedb63c1a
 N
@@ -25,50 +57,30 @@ V
 &plthook/src/main/cpp/bh_dl_iterate.cpp,c/f/cfeec19f05febbafe9195c1ee6a25a8fbd2c0790
 T
 $plthook/src/main/cpp/bh_dl_iterate.h,2/b/2b47387dda894c50aa2756507e52bc8dfc432a5e
+V
+&plthook/src/main/cpp/bh_dl_monitor.cpp,b/b/bbe49ce0950b004f9f7add1c398746e9d79c0290
 T
 $plthook/src/main/cpp/bh_dl_monitor.h,3/a/3a2c51ea1e3baa1652dd3116d1ce2b379ed83481
-X
-(plthook/src/main/cpp/bh_trampo_arm64.cpp,9/f/9fae64d2a89c5bb72f85857a53ad9fd663c9112b
-V
-&plthook/src/main/cpp/bh_trampo_x86.cpp,4/3/436a4967f961d15594c80cfd3d526f64c648aa8b
-Y
-)plthook/src/main/cpp/bh_trampo_x86_64.cpp,7/2/72cf99df954c2b144b9386d5de2833280c956ad4
-P
- plthook/src/main/cpp/bh_util.cpp,5/1/51eda040c8fbe18f026feed9584d90dbe82cbd8c
-V
-&plthook/src/main/cpp/bh_trampo_arm.cpp,f/e/fe992a99254cb14d9973a77bfe268cd230686a7f
-N
-plthook/src/main/cpp/bh_util.h,f/8/f88236864b9fa17e4fcd45bea58e13cec7f14f86
-P
- plthook/src/main/cpp/bytesig.cpp,4/0/402a3874b9eacedd4424ea5b2317fa23a4455e89
-N
-plthook/src/main/cpp/bytesig.h,4/a/4aab1d00beae6879c58b7e828113d88cedca3d5d
-P
- plthook/src/main/cpp/plthook.cpp,d/e/de4430fe0cd9f06596fec9dea443aac7c0d4b3ce
 O
-plthook/src/main/cpp/bh_const.h,2/f/2fee577aab4f94f62bbb7f4b16ff79ef96af96e0
-\
-,plthook/src/main/cpp/third_party/bsd/queue.h,6/7/679f806c3d7fca4c3c581fc50e1a2f5fe8f9aeeb
+plthook/src/main/cpp/bh_elf.cpp,0/a/0aaec3263467fc81155e4ac65cfc527248c5e8ab
+M
+plthook/src/main/cpp/bh_elf.h,6/3/638fa37a59a1a252ebd9f40d25361761af8bbbf2
+W
+'plthook/src/main/cpp/bh_elf_manager.cpp,7/d/7d8d521c658b0a0bb73dd61e1a272309ddb1bd3a
 U
 %plthook/src/main/cpp/bh_elf_manager.h,4/8/48e39b59e5988f5d876b71a838c8f7ce72097387
 N
 plthook/src/main/cpp/bh_hook.h,4/9/4962009932d93121eede0e7268c5c39a436ae8d0
-[
-+plthook/src/main/cpp/third_party/bsd/tree.h,c/2/c2ad0a578c39b513ed33c759167ab3530a2ca500
 P
  plthook/src/main/cpp/bh_hook.cpp,b/9/b9692bb426306bff3c283282e355d94d4ebe6bfa
-O
-plthook/src/main/cpp/bh_cfi.cpp,9/d/9dc3769b9589f3da2ff36a04ef81a0f6392a216d
-M
-plthook/src/main/cpp/bh_elf.h,6/3/638fa37a59a1a252ebd9f40d25361761af8bbbf2
-V
-&plthook/src/main/cpp/bh_dl_monitor.cpp,b/b/bbe49ce0950b004f9f7add1c398746e9d79c0290
-T
-$plthook/src/main/cpp/plthook_jni.cpp,a/d/ad4e50a2e4b324100f1bcf413e5aa87c507f6d48
-O
-plthook/src/main/cpp/bh_elf.cpp,0/a/0aaec3263467fc81155e4ac65cfc527248c5e8ab
+X
+(plthook/src/main/cpp/bh_hook_manager.cpp,3/8/38708321aa3f06e4b292e9e86f9ad46d5f3fb78d
 V
 &plthook/src/main/cpp/bh_hook_manager.h,a/7/a7ed92856f5150526489497f2628f528dc846364
+R
+"plthook/src/main/cpp/bh_linker.cpp,f/e/fe8f962d0a99b254fcf0b5a6c5606eac4e713d7d
+P
+ plthook/src/main/cpp/bh_linker.h,c/2/c2fbfc3f11626da2e9be3d71e068b6c52e089596
 X
 (plthook/src/main/cpp/bh_linker_mutex.cpp,a/a/aaf55b1c0c135a8df677e5ee089a57d2e02207e0
 V
@@ -85,3 +97,7 @@ S
 #plthook/src/main/cpp/bh_sleb128.cpp,e/9/e959c9d1919f88f436dc9063e219109c7d7ed72d
 Q
 !plthook/src/main/cpp/bh_sleb128.h,4/6/46c9e74e63c064d604b7c7e2e34f78e3cd0204f9
+N
+plthook/src/main/cpp/bh_task.h,1/3/1318ea0b5c39bdc5741076ad5ffd97d4d88e12b0
+X
+(plthook/src/main/cpp/bh_task_manager.cpp,7/c/7c5f32790caccaf9c4db209ae0d52c4e6f913bae
--- plthook/src/main/cpp/bh_task.cpp
+++ plthook/src/main/cpp/bh_task.cpp
@@ -3,3 +3,243 @@
 //
 
 #include "bh_task.h"
+
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "bh_core.h"
+#include "bh_elf_manager.h"
+#include "bh_hook_manager.h"
+#include "bh_log.h"
+#include "bh_recorder.h"
+#include "bh_util.h"
+#include "queue.h"
+
+#define BH_TASK_ORIG_FUNC_UNSET   ((void *)0x0)
+#define BH_TASK_ORIG_FUNC_INVALID ((void *)0x1)
+
+static uint32_t bh_task_id_seed = 0;
+
+static bh_task_t *bh_task_create(const char *callee_path_name, const char *sym_name, void *new_func,
+                                 plt_hook_hooked_t hooked, void *hooked_arg) {
+
+    bh_task_t *self;
+    if (NULL == (self = static_cast<bh_task_t *>(malloc(sizeof(bh_task_t))))) {
+        return NULL;
+    }
+
+    self->id = __atomic_fetch_add(&bh_task_id_seed, 1, __ATOMIC_RELAXED);
+    self->callee_path_name = (NULL != callee_path_name ? strdup(callee_path_name) : NULL);
+    self->callee_address = NULL;
+    self->sym_name = strdup(sym_name);
+    self->new_func = new_func;
+    self->hooked = hooked;
+    self->hooked_arg = hooked_arg;
+    self->hook_status_code = PLT_HOOK_STATUS_CODE_MAX;
+    self->manual_orig_func = BH_TASK_ORIG_FUNC_UNSET;
+
+    return self;
+}
+
+
+bh_task_t *bh_task_create_single(const char *caller_path_name, const char *callee_path_name,
+                                 const char *sym_name, void *new_func, plt_hook_hooked_t hooked,
+                                 void *hooked_arg) {
+
+    bh_task_t *self;
+    if (NULL == (self = bh_task_create(callee_path_name, sym_name, new_func, hooked, hooked_arg))) {
+        return NULL;
+    }
+    self->type = BH_TASK_TYPE_SINGLE;
+    self->status = BH_TASK_STATUS_UNFINISHED;
+    self->caller_path_name = (NULL != caller_path_name ? strdup(caller_path_name) : NULL);
+    return self;
+}
+
+
+bh_task_t *bh_task_create_partial(plt_hook_caller_allow_filter_t caller_allow_filter,
+                                  void *caller_allow_filter_arg, const char *callee_path_name,
+                                  const char *sym_name, void *new_func, plt_hook_hooked_t hooked,
+                                  void *hooked_arg) {
+    bh_task_t *self;
+    if (NULL == (self = bh_task_create(callee_path_name, sym_name, new_func, hooked, hooked_arg))) {
+        return NULL;
+    }
+    self->type = BH_TASK_TYPE_PARTIAL;
+    self->status = BH_TASK_STATUS_LONGTERM;
+    self->caller_path_name = NULL;
+    self->caller_allow_filter = caller_allow_filter;
+    self->caller_allow_filter_arg = caller_allow_filter_arg;
+    return self;
+}
+
+bh_task_t *bh_task_create_all(const char *callee_path_name, const char *sym_name, void *new_func,
+                              plt_hook_hooked_t hooked, void *hooked_arg) {
+
+    bh_task_t *self;
+    if (NULL == (self = bh_task_create(callee_path_name, sym_name, new_func, hooked, hooked_arg))) {
+        return NULL;
+    }
+    self->type = BH_TASK_TYPE_ALL;
+    self->status = BH_TASK_STATUS_LONGTERM;
+    self->caller_path_name = NULL;
+    return self;
+}
+
+void bh_task_destroy(bh_task_t **self) {
+    if (NULL == self || NULL == *self) {
+        return;
+    }
+
+    if (NULL != (*self)->caller_path_name) {
+        free((*self)->caller_path_name);
+    }
+    if (NULL != (*self)->callee_path_name) {
+        free((*self)->callee_path_name);
+    }
+    if (NULL != (*self)->sym_name) {
+        free((*self)->sym_name);
+    }
+    free(*self);
+    *self = NULL;
+}
+
+static bool bh_task_hook_or_unhook(bh_task_t *self, bh_elf_t *elf) {
+    void (*hook_or_unhook)(bh_hook_manager_t *, bh_task_t *, bh_elf_t *) =
+    (BH_TASK_STATUS_UNHOOKING == self->status ? bh_hook_manager_unhook : bh_hook_manager_hook);
+
+    switch (self->type) {
+        case BH_TASK_TYPE_SINGLE:
+            if (bh_elf_is_match(elf, self->caller_path_name)) {
+                hook_or_unhook(bh_core_global()->hook_mgr, self, elf);
+                if (BH_TASK_STATUS_UNHOOKING != self->status) {
+                    self->status = BH_TASK_STATUS_FINISHED;
+                }
+                return false;
+            }
+            return true;
+        case BH_TASK_TYPE_PARTIAL:
+            if (self->caller_allow_filter(elf->pathname, self->caller_allow_filter_arg)) {
+                hook_or_unhook(bh_core_global() -> hook_mgr, self, elf);
+            }
+            return true;
+        case BH_TASK_TYPE_ALL:
+            hook_or_unhook(bh_core_global()->hook_mgr, self, elf);
+            return true;
+    }
+}
+
+
+static bool bh_task_elf_iterate_cb(bh_elf_t *elf, void *arg) {
+    return bh_task_hook_or_unhook((bh_task_t *)arg, elf);
+}
+
+static void bh_task_handle(bh_task_t *self) {
+    switch (self->type) {
+        case BH_TASK_TYPE_SINGLE: {
+            bh_elf_t *caller_elf = bh_elf_manager_find_elf(bh_core_global()->elf_mgr, self->caller_path_name);
+            if (NULL != caller_elf) {
+                bh_task_hook_or_unhook(self, caller_elf);
+            }
+            break;
+        }
+        case BH_TASK_TYPE_ALL:
+        case BH_TASK_TYPE_PARTIAL:
+            bh_elf_manager_iterate(bh_core_global()->elf_mgr, bh_task_elf_iterate_cb, (void *)self);
+            break;
+    }
+}
+
+static int bh_task_check_pre_hook(bh_task_t *self) {
+    if (BH_TASK_STATUS_FINISHED == self->status) {
+        return -1;
+    }
+
+    if (NULL != self->callee_path_name && NULL == self->callee_address) {
+        self->callee_address = bh_elf_manager_find_export_address(bh_core_global()->elf_mgr, self->callee_path_name, self->sym_name);
+        if (NULL == self->callee_address) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+
+void bh_task_hook(bh_task_t *self) {
+    if (0 != bh_task_check_pre_hook(self)) {
+        return;
+    }
+    bh_task_handle(self);
+}
+
+void bh_task_hook_elf(bh_task_t *self, bh_elf_t *elf) {
+    if (0 != bh_task_check_pre_hook(self)) {
+        return;
+    }
+
+    bh_task_hook_or_unhook(self, elf);
+}
+
+int bh_task_unhook(bh_task_t *self) {
+    self->status = BH_TASK_STATUS_UNHOOKING;
+
+    if (PLT_HOOK_MODE_MANUAL == bh_core_get_mode()) {
+        if (BH_TASK_ORIG_FUNC_UNSET == self->manual_orig_func) {
+            return PLT_HOOK_STATUS_CODE_OK;
+        } else if (BH_TASK_ORIG_FUNC_INVALID == self->manual_orig_func) {
+            BH_LOG_WARN("task: try to unhook with unmatch original function in manual mode");
+            return PLT_HOOK_STATUS_CODE_UNMATCH_ORIG_FUNC;
+        }
+    }
+    bh_task_handle(self);
+    return PLT_HOOK_STATUS_CODE_OK;
+}
+
+void bh_task_set_manual_orig_func(bh_task_t *self, void *orig_func) {
+    if (NULL == orig_func || BH_TASK_ORIG_FUNC_INVALID == orig_func) {
+        return;
+    }
+
+    if (BH_TASK_ORIG_FUNC_UNSET == self->manual_orig_func) {
+        self->manual_orig_func = orig_func;
+    } else if (BH_TASK_ORIG_FUNC_INVALID == self->manual_orig_func) {
+        return;
+    } else {
+        if (self->manual_orig_func != orig_func) {
+            self->manual_orig_func = BH_TASK_ORIG_FUNC_INVALID;
+        }
+    }
+}
+
+void *bh_task_get_manual_orig_func(bh_task_t *self) {
+    if (BH_TASK_ORIG_FUNC_UNSET == self->manual_orig_func) {
+        return NULL;
+    }
+    if (BH_TASK_ORIG_FUNC_INVALID == self->manual_orig_func) {
+        return NULL;
+    }
+
+    return self->manual_orig_func;
+}
+
+void bh_task_hooked(bh_task_t *self, int status_code, const char *caller_path_name, void *orig_func) {
+    // single type task always with a caller_path_name
+    if (BH_TASK_TYPE_SINGLE == self->type && NULL == caller_path_name) {
+        caller_path_name = self->caller_path_name;
+    }
+
+    // save hook-status-code for single-task
+    if (PLT_HOOK_STATUS_CODE_ORIG_ADDR != status_code && BH_TASK_TYPE_SINGLE == self->type &&
+        BH_TASK_STATUS_UNHOOKING != self->status) {
+        self->hook_status_code = status_code;
+    }
+
+    // do callback
+    if (NULL != self->hooked && BH_TASK_STATUS_UNHOOKING != self->status) {
+        self->hooked(self, status_code, caller_path_name, self->sym_name, self->new_func, orig_func,
+                     self->hooked_arg);
+    }
+}
